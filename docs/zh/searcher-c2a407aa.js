50563dfe9665e87014ac0ae9ba7970c84709e12f
Yue Chen <dspfac@gmail.com>
2026-04-23 10:45:57 -0700

    Expand long-context theory and sourcing

--- docs/zh/searcher-c2a407aa.js
+++ docs/zh/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a008ad06.js',
+                path_to_root + 'searchindex-4fa2a8b1.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
