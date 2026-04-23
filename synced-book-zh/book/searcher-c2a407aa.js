2608626aace47fb86b3072dee30df993af10aa62
Yue Chen <dspfac@gmail.com>
2026-04-23 10:52:38 -0700

    Convert chapter 2 citations to footnotes

--- synced-book-zh/book/searcher-c2a407aa.js
+++ synced-book-zh/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4fa2a8b1.js',
+                path_to_root + 'searchindex-87d8682b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
