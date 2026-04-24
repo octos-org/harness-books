b4c78ee8101d2c4de115e67d9dcaa4a6093ab221
Yue Chen <dspfac@gmail.com>
2026-04-23 23:27:49 -0700

    Unify architecture language across chapters 4 and 9

--- synced-book-zh/book/searcher-c2a407aa.js
+++ synced-book-zh/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2776454b.js',
+                path_to_root + 'searchindex-579ff624.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
