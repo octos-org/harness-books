fdec8c9fc083c788ac915eec3a8163c3d1089120
Yue Chen <dspfac@gmail.com>
2026-04-23 21:55:38 -0700

    Publish chapter 1 runtime bridge

--- docs/zh/searcher-c2a407aa.js
+++ docs/zh/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8bc89d02.js',
+                path_to_root + 'searchindex-5f276ace.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
